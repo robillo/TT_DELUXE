4fe45eb3a303c4f143aeb9b3ce53c0c1a15464f3
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-14 01:11:44 +0530

    implementing tts class

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/ui/tts.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/ui/tts.java
@@ -1,8 +1,21 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.ui;
 
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
 /**
  * Created by robinkamboj on 14/12/16.
  */
 
-public class tts {
+public class tts implements TextToSpeech.OnInitListener{
+
+    public TextToSpeech tts;
+
+    @Override
+    public void onInit(int status) {
+        if(status!= TextToSpeech.SUCCESS){
+            Log.d("InitTextToSpeech", "init text to speech failed; status: " + status);
+            tts=null;
+        }
+    }
 }
