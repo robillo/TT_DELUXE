2e3b0cee0c7077b972239b2ea807e4e71939006c
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-22 17:48:00 +0530

    ...

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/DetailActivity.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/DetailActivity.java
@@ -1,15 +1,48 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firstapp.robinpc.tongue_twisters_deluxe.R;
+import com.firstapp.robinpc.tongue_twisters_deluxe.controller.Recycler_View_Adapter;
+import com.firstapp.robinpc.tongue_twisters_deluxe.controller.RecyclerviewTouchListener;
+import com.firstapp.robinpc.tongue_twisters_deluxe.model.Data;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class DetailActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
+
+
+    CoordinatorLayout coordinatorLayout;
+    TextView mainText, header1, header2;
+    Context context;
+    private TextToSpeech tts;
+    private int level_number;
+    private String[] level_headers, level_twisters;
+    List<Data> data;
+    ListIterator<Data> dataListIterator;
+    Button prev, next;
+    Data test_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +51,319 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        prev = (Button) findViewById(R.id.prev);
+        next = (Button) findViewById(R.id.next);
+
+        header1 = (TextView) findViewById(R.id.header1);
+        header2 = (TextView) findViewById(R.id.header2);
+        mainText = (TextView) findViewById(R.id.mainText);
+        context = getApplicationContext();
+
+        Intent i = getIntent();
+        level_number = i.getIntExtra("level_number", 1);
+
+        inflateInfo();
+
+        data = fillWithData();
+        dataListIterator = data.listIterator();
+
+        test_data = dataListIterator.next();
+
+        header1.setText(test_data.Title);
+        header2.setText(test_data.subTitle);
+        mainText.setText(test_data.TT);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager myLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(myLayoutManager);
+
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(data, getApplicationContext());
+        recyclerView.setAdapter(adapter);
+
+        //for RV Touch Listener
+        recyclerView.addOnItemTouchListener(new RecyclerviewTouchListener(context, recyclerView, new RecyclerviewTouchListener.ClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v, int position) {
+                //Toast.makeText(context, "ROBIN " + position, Toast.LENGTH_SHORT).show();
+                //to take dataListIterator to 0 index
+                while (dataListIterator.hasPrevious()){
+                    test_data = dataListIterator.previous();
+                }
+
+                if(position == 0){
+                    header1.setText(test_data.Title);
+                    header2.setText(test_data.subTitle);
+                    mainText.setText(test_data.TT);
+                    prev.setEnabled(false);
+                    if(!next.isEnabled())
+                        next.setEnabled(true);
+                }
+                else {
+                    for(int i = 0; i <= position; i++){
+                        test_data = dataListIterator.next();
+                    }
+                    header1.setText(test_data.Title);
+                    header2.setText(test_data.subTitle);
+                    mainText.setText(test_data.TT);
+                    if(position < 14){
+                        if(!prev.isEnabled())
+                            prev.setEnabled(true);
+                        if(!next.isEnabled())
+                            next.setEnabled(true);
+                    }
+                    if(position == 14){
+                        if(!prev.isEnabled())
+                            prev.setEnabled(true);
+                        if(next.isEnabled())
+                            next.setEnabled(false);
+                    }
+                }
+
             }
-        });
+
+            @Override
+            public void onLongClick(View v, int position) {
+                //Toast.makeText(context, "ROBIN ", Toast.LENGTH_SHORT).show();
+                while (dataListIterator.hasPrevious()){
+                    test_data = dataListIterator.previous();
+                }
+
+                if(position == 0){
+                    header1.setText(test_data.Title);
+                    header2.setText(test_data.subTitle);
+                    mainText.setText(test_data.TT);
+                    prev.setEnabled(false);
+                    if(!next.isEnabled())
+                        next.setEnabled(true);
+                }
+                else {
+                    for(int i = 0; i <= position; i++){
+                        test_data = dataListIterator.next();
+                    }
+                    header1.setText(test_data.Title);
+                    header2.setText(test_data.subTitle);
+                    mainText.setText(test_data.TT);
+                    if(position < 14){
+                        if(!prev.isEnabled())
+                            prev.setEnabled(true);
+                        if(!next.isEnabled())
+                            next.setEnabled(true);
+                    }
+                    if(position == 14){
+                        if(!prev.isEnabled())
+                            prev.setEnabled(true);
+                        if(next.isEnabled())
+                            next.setEnabled(false);
+                    }
+                }
+            }
+        }));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+
+        switch (id){
+            case R.id.action_settings:{
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                break;
+            }
+            case R.id.action_stop_tts:{
+                if(tts.isSpeaking()){
+                    tts.stop();
+                    Snackbar.make(coordinatorLayout, "TTS Stopped.", Snackbar.LENGTH_SHORT).show();
+                }
+                else {
+                    Snackbar.make(coordinatorLayout, "TTS is already off.", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void inflateInfo() {
+        switch (level_number){
+            case 1:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.one);
+                break;
+            }
+            case 2:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.two);
+                break;
+            }
+            case 3:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.three);
+                break;
+            }
+            case 4:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.four);
+                break;
+            }
+            case 5:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.five);
+                break;
+            }
+            case 6:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.six);
+                break;
+            }
+            case 7:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.seven);
+                break;
+            }
+            case 8:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.eight);
+                break;
+            }
+            case 9:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.nine);
+                break;
+            }
+            case 10:{
+                level_headers = getResources().getStringArray(R.array.one_headers);
+                level_twisters = getResources().getStringArray(R.array.ten);
+                break;
+            }
+        }
+    }
+
+    private List<Data> fillWithData() {
+        List<Data> data = new ArrayList<>();
+
+        for (int i = 1; i <= 15; i++) {
+            data.add(new Data("LEVEL " + level_number + " TT " + i, level_headers[i - 1], level_twisters[i - 1]));
+        }
+        return data;
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.fab: {
+                //progressBar.setVisibility(View.VISIBLE);
+                //progressBar.setIndeterminate(true);
+                if (tts.isSpeaking()) {
+                    tts.stop();
+                }
+                autoSpeak((String) mainText.getText());
+                /*while (tts.isSpeaking()){
+                    stop_tts.setVisibility(View.VISIBLE);
+                }*/
+
+                //above part supposed to be in a thread
+
+                break;
+            }
+
+            case R.id.prev: {
+                if (dataListIterator.hasPrevious()) {
+                    test_data = dataListIterator.previous();
+                    if (test_data.Title != null) {
+                        if (!next.isEnabled()) {
+                            next.setEnabled(true);
+                        }
+                        header1.setText(test_data.Title);
+                        header2.setText(test_data.subTitle);
+                        mainText.setText(test_data.TT);
+                    }
+                } else {
+                    dataListIterator.next();
+                    dataListIterator.next();
+                    prev.setEnabled(false);
+                    Toast.makeText(context, "No previous element", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case R.id.next: {
+                if (dataListIterator.hasNext()) {
+                    test_data = dataListIterator.next();
+                    if (test_data.Title != null) {
+                        if (!prev.isEnabled()) {
+                            prev.setEnabled(true);
+                        }
+                        header1.setText(test_data.Title);
+                        header2.setText(test_data.subTitle);
+                        mainText.setText(test_data.TT);
+                    }
+                } else {
+                    dataListIterator.previous();
+                    dataListIterator.previous();
+                    next.setEnabled(false);
+                    Toast.makeText(context, "No next element", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    private void autoSpeak(String text) {
+        if (TextUtils.isEmpty(text) || tts == null) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_ADD, null, "SpeakText");
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_ADD, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts.isSpeaking()) {
+            tts.stop();
+        }
+        super.onDestroy();
+
+        tts.shutdown();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status != TextToSpeech.SUCCESS) {
+            Log.d("InitTextToSpeech", "init text to speech failed; status: " + status);
+            tts = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if (tts.isSpeaking()) {
+            tts.stop();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+
+        tts = new TextToSpeech(DetailActivity.this, DetailActivity.this);
+
+        super.onStart();
+    }
 }
--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/LevelsActivity.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/LevelsActivity.java
@@ -1,15 +1,59 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.firstapp.robinpc.tongue_twisters_deluxe.R;
+import com.firstapp.robinpc.tongue_twisters_deluxe.controller.ZoomOutPageTransformer;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide10Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide2Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide3Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide4Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide5Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide6Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide7Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide8Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.Slide9Fragment;
+import com.firstapp.robinpc.tongue_twisters_deluxe.view.slides.SlideFragment;
 
 public class LevelsActivity extends AppCompatActivity {
+
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 10;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +62,184 @@ public class LevelsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.fragment_slide,
+                R.layout.fragment_slide2,
+                R.layout.fragment_slide3,
+                R.layout.fragment_slide4,
+                R.layout.fragment_slide5,
+                R.layout.fragment_slide6,
+                R.layout.fragment_slide7,
+                R.layout.fragment_slide8,
+                R.layout.fragment_slide9,
+                R.layout.fragment_slide10};
+
+        // adding bottom dots
+        addBottomDots(0);
+
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setClipToPadding(false);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mPager.addOnPageChangeListener(viewPagerPageChangeListener);
     }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_levels, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        private LayoutInflater layoutInflater;
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:{
+                    return new SlideFragment();
+                }
+                case 1:{
+                    return new Slide2Fragment();
+                }
+                case 2:{
+                    return new Slide3Fragment();
+                }
+                case 3:{
+                    return new Slide4Fragment();
+                }
+                case 4:{
+                    return new Slide5Fragment();
+                }
+                case 5:{
+                    return new Slide6Fragment();
+                }
+                case 6:{
+                    return new Slide7Fragment();
+                }
+                case 7:{
+                    return new Slide8Fragment();
+                }
+                case 8:{
+                    return new Slide9Fragment();
+                }
+                case 9:{
+                    return new Slide10Fragment();
+                }
+                default:{
+                    return new SlideFragment();
+                }
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return 1.0f;
+        }
+
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                //btnNext.setText(getString(R.string.start));
+                //btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                //btnNext.setText(getString(R.string.next));
+                //btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
 
 }
