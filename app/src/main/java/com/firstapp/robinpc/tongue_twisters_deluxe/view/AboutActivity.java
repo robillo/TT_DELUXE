c3b2cf546195c25447c3d3c801d515f742520b52
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-22 20:51:36 +0530

    ...

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/AboutActivity.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.view;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -14,5 +15,17 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        setupActionBar();
     }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
 }
