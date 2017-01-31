730528927d9ee0fab0382c03b859777d02acc086
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-31 15:37:33 +0530

    menu_your

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/YourActivity.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/YourActivity.java
@@ -1,10 +1,14 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -42,4 +46,42 @@ public class YourActivity extends AppCompatActivity {
                 .into(imageView3);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_your, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_settings:{
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                break;
+            }
+            case R.id.action_about:{
+                Intent i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                break;
+            }
+            case R.id.action_rate:{
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.firstapp.robinpc.tongue_twisters_deluxe"));
+                startActivity(i);
+                break;
+            }
+            case R.id.action_share:{
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_SEND);
+                i.putExtra(Intent.EXTRA_TEXT, "Hey, Check out this exciting App at: https://play.google.com/store/apps/details?id=com.firstapp.robinpc.tongue_twisters_deluxe");
+                i.setType("text/plain");
+                startActivity(i);
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
