b54d75a3c09c9c597958c37a438257d93fe28ff3
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-08 11:01:53 +0530

    widget

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/widget/NewAppWidget.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/widget/NewAppWidget.java
@@ -9,14 +9,13 @@ import com.firstapp.robinpc.tongue_twisters_deluxe.R;
 
 /**
  * Implementation of App Widget functionality.
- * App Widget Configuration implemented in {@link NewAppWidgetConfigureActivity NewAppWidgetConfigureActivity}
  */
 public class NewAppWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = NewAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
@@ -30,14 +29,6 @@ public class NewAppWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        // When the user deletes the widget, delete the preference associated with it.
-        for (int appWidgetId : appWidgetIds) {
-            NewAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
         }
     }
 
