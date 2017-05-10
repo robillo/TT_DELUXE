7f0ecd6d14abc2664d5d977fd6d533be56eb64f2
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-05-10 08:32:52 +0530

    slides now using include layout.

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/slides/Slide3Fragment.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/view/slides/Slide3Fragment.java
@@ -4,11 +4,13 @@ package com.firstapp.robinpc.tongue_twisters_deluxe.view.slides;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -19,9 +21,6 @@ import com.firstapp.robinpc.tongue_twisters_deluxe.view.activities.DetailActivit
  * A simple {@link Fragment} subclass.
  */
 public class Slide3Fragment extends Fragment {
-
-    ImageView imageView1;
-    Button detail;
 
     public Slide3Fragment() {
         // Required empty public constructor
@@ -34,16 +33,20 @@ public class Slide3Fragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_slide3, container, false);
 
-        imageView1 = (ImageView) v.findViewById(R.id.imageView1);
-        detail = (Button) v.findViewById(R.id.detail);
+        CardView cardView = (CardView) v.findViewById(R.id.cardView);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
+        TextView level = (TextView) v.findViewById(R.id.level);
+        level.setText(getResources().getString(R.string.slide_1_title));
+        TextView level_header = (TextView) v.findViewById(R.id.level_header);
+        level_header.setText(getResources().getString(R.string.slide_1_desc));
         Glide.with(this)
                 .load("https://1.bp.blogspot.com/-OYwFn64dzV0/WJBfWvYaXaI/AAAAAAAAAFc/1egquqeEF30HrOgarJflAGp8fXqyBeKDQCLcB/s1600/ph4.jpg")
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .crossFade()
                 .centerCrop()
-                .into(imageView1);
+                .into(imageView);
 
-        detail.setOnClickListener(new View.OnClickListener() {
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
