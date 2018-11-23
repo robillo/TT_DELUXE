7fee96f14e021d36693a34dcc724085c1b980f1e
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-11-23 19:25:40 +0530

    redesigned icon, removed previous version redundant res, decreased app size to 2.5M

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/controller/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.firstapp.robinpc.tongue_twisters_deluxe.controller;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
