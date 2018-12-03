f4b5b6f3628c5080724abf46f1b81bbd65e89c9f
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-12-04 00:11:48 +0530

    rate app button. developers and content creators credits

--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/utils/AppConstants.java
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/utils/AppConstants.java
@@ -5,4 +5,8 @@ public class AppConstants {
     public static final String PREFERENCE_FILE_NAME = "TWISTERS_DELUXE_PREFERENCES";
     public static final String PREFERENCE_IS_AUTO_PLAY_ON = "IS_AUTO_PLAY_ON";
     public static final String CONSTANT_UTTERANCE_ID = "UTTERANCE_ID";
+
+    public static final String URL_AMIT_CONTENT_CREATOR_PROFILE = "https://2.bp.blogspot.com/-uw1DbSACs4s/XAVscieg-MI/AAAAAAAADbA/PiNbZQtn0D8uGmuNM4DPGdhUCULIV-PpgCK4BGAYYCw/s1600/WhatsApp%2BImage%2B2018-12-03%2Bat%2B23.12.56.jpeg";
+    public static final String URL_ANUBHAV_CONTENT_CREATOR_PROFILE = "https://4.bp.blogspot.com/-vn2ZLXAQUpY/XAVsb1kdbAI/AAAAAAAADa4/wWV2Ax4gIzENsDc6vpen0ozkVlR6qvUnQCK4BGAYYCw/s1600/WhatsApp%2BImage%2B2018-12-03%2Bat%2B23.14.33.jpeg";
+    public static final String URL_ROBIN_DEVELOPER_PROFILE = "https://2.bp.blogspot.com/-edkGKZ-_0_E/XAVsauEs41I/AAAAAAAADaw/vrxNeFzEfQEzKsEzxUjvRmUU2qMZWcSRQCK4BGAYYCw/s1600/WhatsApp%2BImage%2B2018-12-03%2Bat%2B23.15.03.jpeg";
 }
