12d8020e221d53d0bf961ef205071a899904cab5
Robin Kamboj <dev.robinkamboj@gmail.com>
2019-06-26 01:34:45 +0530

    starting with app redesign

---
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/ui/home/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.firstapp.robinpc.tongue_twisters_deluxe.ui.home
+
+import android.content.Context
+import android.content.Intent
+import com.firstapp.robinpc.tongue_twisters_deluxe.R
+import com.firstapp.robinpc.tongue_twisters_deluxe.ui.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, HomeActivity::class.java)
+        }
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_home
+    }
+
+    override fun setup() {
+
+    }
+
+    override fun onBackPressed() {
+        val goHome = Intent(Intent.ACTION_MAIN)
+        goHome.addCategory(Intent.CATEGORY_HOME)
+        goHome.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(goHome)
+    }
+}
--- app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/firstapp/robinpc/tongue_twisters_deluxe/ui/splash/SplashActivity.kt
@@ -1,18 +1,46 @@
 package com.firstapp.robinpc.tongue_twisters_deluxe.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.firstapp.robinpc.tongue_twisters_deluxe.R
 import com.firstapp.robinpc.tongue_twisters_deluxe.ui.base.BaseActivity
+import com.firstapp.robinpc.tongue_twisters_deluxe.ui.home.HomeActivity
 
 class SplashActivity : BaseActivity() {
+
+    companion object {
+        private const val STEP_VALUE = 500L
+        private const val TIMER_VALUE = 3000L
+    }
 
     override fun getLayoutResId(): Int {
         return R.layout.activity_splash
     }
 
     override fun setup() {
-
+        startTimer()
     }
 
+    private fun startTimer() {
+        val timer = object: CountDownTimer(TIMER_VALUE, STEP_VALUE) {
+            override fun onFinish() {
+                startHomeActivity()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                //TODO
+            }
+        }
+        timer.start()
+    }
+
+    private fun getCompletionPercentage(remainingTime: Long): Int {
+        val timeElapsed = TIMER_VALUE - remainingTime
+        return timeElapsed.toInt() * 100 / TIMER_VALUE.toInt()
+    }
+
+    private fun startHomeActivity() {
+        startActivity(HomeActivity.newIntent(this))
+        animateActivityTransition(R.anim.slide_in_right_activity, R.anim.slide_out_left_activity)
+    }
 }
